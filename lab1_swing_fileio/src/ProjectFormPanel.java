44ac3d0da9427a2278505b6748b5546f97c78733
Unay <dotokafu@gmail.com>
2026-03-02 12:45:41 +0300

    Implement ProjectFormPanel

--- lab1_swing_fileio/src/ProjectFormPanel.java
+++ lab1_swing_fileio/src/ProjectFormPanel.java
@@ -1,5 +1,31 @@
 package lab1_swing_fileio.src;
 
-public class ProjectFormPanel {
-    
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.awt.*;
+
+public class  ProjectFormPanel extends JLabel{
+    public static JPanel main(String arr[]){
+        
+        JPanel panel = new JPanel(new GridLayout(1,5,5,5));
+        panel.add(new JLabel("Project Name "));
+        panel.add(new JTextField());
+        panel.add(new JLabel("Team"));
+        panel.add(new JTextField());
+        panel.add(new JLabel("Team Size "));
+        panel.add(new JComboBox<>());
+        panel.add(new JLabel("ProjectType"));
+        panel.add(new JComboBox<>());
+        panel.add(new JLabel("StartDate"));
+        panel.add(new JTextField());
+
+        
+
+
+        return panel;
+    }
+   
 }
