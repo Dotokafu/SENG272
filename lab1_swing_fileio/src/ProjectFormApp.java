ab614bbbd02917de03da16b9ac63b80c4f225934
Unay <dotokafu@gmail.com>
2026-03-02 12:17:06 +0300

    ProjectFormApp implementation

--- lab1_swing_fileio/src/ProjectFormApp.java
+++ lab1_swing_fileio/src/ProjectFormApp.java
@@ -1,5 +1,16 @@
 package lab1_swing_fileio.src;
 
+import javax.swing.JFrame;
+
+
 public class ProjectFormApp {
+    public static void main(String arr[]){
+        JFrame frame =new JFrame("Software Project Registration Form");
+        frame.setSize(500,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Add instence of ProjectFormPanel
+        frame.setVisible(true);
+    }
     
+
 }
