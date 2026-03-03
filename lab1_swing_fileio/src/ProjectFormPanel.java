e2b74d5f0fed27a21cd0df85dc7a20e3375de265
Unay <dotokafu@gmail.com>
2026-03-03 09:42:43 +0300

    Handle Getting Input

--- lab1_swing_fileio/src/ProjectFormPanel.java
+++ lab1_swing_fileio/src/ProjectFormPanel.java
@@ -7,30 +7,49 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class  ProjectFormPanel extends JPanel{
     public ProjectFormPanel(){
         
         setLayout(new GridLayout(0, 2, 5, 5));
         add(new JLabel("Project Name "));
-        add(new JTextField());
+        JTextField projectName = new JTextField();
+        add(projectName);
 
         add(new JLabel("Team"));
-        add(new JTextField());
+        JTextField Team = new JTextField();
+        add(Team);
 
         add(new JLabel("Team Size "));
-        add(new JComboBox<>());
+        JComboBox TeamSize = new JComboBox<>(new String[]{"1-3","4-6","7-10","10+"});
+        add(TeamSize);
 
         add(new JLabel("ProjectType"));
-        add(new JComboBox<>());
+        JComboBox ProjectType = new JComboBox<>(new String[]{"Web","Mobile","Desktop","API"});
+        add(ProjectType);
 
         add(new JLabel("StartDate"));
-        add(new JTextField());
-        JButton SaveButton =new JButton("Save");
+        JTextField StartDate = new JTextField();
+        add(StartDate);
+
         JButton ClearButton =new JButton("Clear");
+        JButton SaveButton =new JButton("Save");
+        
         add(SaveButton);
         add(ClearButton);
-
+        SaveButton.addActionListener(e->{
+            String ProjectName = projectName.getText();
+            String TeamName = Team.getText();
+            String teamSize = (String) TeamSize.getSelectedItem();
+            String projectType = (String) ProjectType.getSelectedItem();
+            String startDate = StartDate.getText();
+        });
+      
 
         
 
