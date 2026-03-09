c168bd8cd3526edfb2ad2cfc04988f2d8a3e322d
Unay <dotokafu@gmail.com>
2026-03-09 15:00:32 +0300

    reupload lab1i

---
+++ Lab1/lab1_swing_fileio/src/ProjectFormApp.java
@@ -0,0 +1,21 @@
+package lab1_swing_fileio.src;
+
+import javax.swing.JFrame;
+
+import javax.swing.JPanel;
+
+
+public class ProjectFormApp {
+    public static void main(String arr[]){
+        JFrame frame =new JFrame("Software Project Registration Form");
+        frame.setSize(500,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JPanel panel = new ProjectFormPanel();
+        frame.add(panel);
+        
+        frame.setVisible(true);
+    }
+    
+
+}
