8d44deebf4d3185c0bb5db37ec313afa56d63c90
Unay <dotokafu@gmail.com>
2026-03-10 11:34:00 +0300

    main implementation

--- Lab2/Lab2-sw-quality/src/Main.java
+++ Lab2/Lab2-sw-quality/src/Main.java
@@ -1,3 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
-    
-}
+    public static void main(String[] args) {
+        HashMap<String, ArrayList<SWSystem>> allSystems = SWSystemData.getAllSystems();
+
+        ArrayList<SWSystem> webSystems = allSystems.get("Web");
+        SWSystem shopSphere = null;
+        for (SWSystem system : webSystems) {
+            if (system.getName().equals("ShopSphere")) {
+            shopSphere = system;
+            break;}
+        }
+        shopSphere.getDimensions().get(0).getCriteria().get(0).setMeasuredValue(94.0);
+        shopSphere.getDimensions().get(0).getCriteria().get(1).setMeasuredValue(91.0);
+
+        shopSphere.getDimensions().get(1).getCriteria().get(0).setMeasuredValue(99.2);
+        shopSphere.getDimensions().get(1).getCriteria().get(1).setMeasuredValue(2.1);
+
+        shopSphere.getDimensions().get(2).getCriteria().get(0).setMeasuredValue(220.0);
+        shopSphere.getDimensions().get(2).getCriteria().get(1).setMeasuredValue(38.0);
+
+        shopSphere.getDimensions().get(3).getCriteria().get(0).setMeasuredValue(72.0);
+        shopSphere.getDimensions().get(3).getCriteria().get(1).setMeasuredValue(8.5);
+
+        shopSphere.printReport();
+
+}}
