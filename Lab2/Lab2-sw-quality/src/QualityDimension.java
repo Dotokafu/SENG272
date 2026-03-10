c23cf2cc9360a8c7e55cd0c9d0fd2488adfaf654
Unay <dotokafu@gmail.com>
2026-03-10 09:47:04 +0300

    add qualityDimensions metods

--- Lab2/Lab2-sw-quality/src/QualityDimension.java
+++ Lab2/Lab2-sw-quality/src/QualityDimension.java
@@ -12,7 +12,42 @@ public class QualityDimension {
         this.weight =weight;
         this.criteria = new ArrayList<>();
     }
-    public void addCriterion(){
+    public void addCriterion(Criterion criterion){
+        criteria.add(criterion);
         
+    }
+    public double calculateDimensionScore(){
+        if (criteria.isEmpty()){return 0.0;}
+        double weightSum=0;
+        double metricSum=0;
+
+        for(Criterion c: criteria){
+            weightSum +=c.getWeight();
+            metricSum += (c.calculateScore() *c.getWeight());
+        }
+        return metricSum/weightSum;
+    }
+    public String getQualityLabel(){
+        double score =calculateDimensionScore();
+        if(score>=4.5){
+            return "Excellent Quality";
+        }
+        else if(score>=3.5){return "Good Quality";}
+        else if(score>=2.5){return "Needs Improvement";}
+        else{return "Poor Quality";}
+        
+    }
+    public double getQualityGap(){
+        return 5.0 - calculateDimensionScore();
+    }
+
+    public String getName(){return name;}
+    public String getIsoCode(){return isoCode;}
+    public double getWeight(){return weight;}
+    public ArrayList<Criterion> getCriteria(){return criteria;}
+
+    public String toString(){
+        return String.format("--- %s  [%s] (Weight: %.0f) ---", name,isoCode,weight);
+
     }
 }
